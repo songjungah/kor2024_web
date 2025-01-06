547b6163f587454fa2aef4c550a508f2ee8589bb
songjungah <1104wjddk@naver.com>
2025-01-06 19:00:25 +0900

    250106 학원정리

---
+++ src/main/java/day54/boardservice13/controller/BoardController.java
@@ -0,0 +1,103 @@
+package day54.boardservice13.controller;
+
+import day54.boardservice13.model.BoardDao;
+import day54.boardservice13.model.BoardDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+
+// ========== 콘솔 ========== //
+//public class BoardController {
+//
+//    // 싱글톤
+//    private static BoardController instance = new BoardController();
+//    private BoardController(){}
+//    public static BoardController getInstance() {
+//        return instance;
+//    }
+//
+//    // 1. 게시물 쓰기        [CRUD 중에 C, HTTP METHOD 중에 POST]
+//    public boolean write(BoardDto boardDto) {
+//        boolean result = BoardDao.getInstance().write(boardDto);
+//        return result;
+//    }
+//
+//    // 2. 게시물 전체 조회     [CRUD 중에 R, HTTP METHOD 중에 GET]
+//    public ArrayList<BoardDto> findAll() {
+//        ArrayList<BoardDto> result = BoardDao.getInstance().findAll();
+//        return result;
+//    }
+//
+//    // 3. 게시물 개별 조회
+//
+//
+//    // 4. 게시물 수정
+//
+//
+//    // 5. 게시물 삭제
+//
+//
+//}
+
+// ========== 웹 ========== //
+@RestController     // 현재 클래스에 rest api 가 가능한 클래스임을 주입한다.
+// [1] 클래스 위에 @RestController 넣기
+public class BoardController {
+    // 1. 게시물 쓰기        [CRUD 중에 C, HTTP METHOD 중에 POST]
+    // [2] 함수위에 @XXXMapping("/http 주소 만들기")
+    // [TEST] Talend Api Tester : [POST] http://localhost:8080/write
+    //                            [Headers] Content-Type : application/json
+    //                            [body] {"btitle" : "테스트제목" , "bcontent" : "테스트내용" , "bwriter" : "유재석" ,"bpwd" : "1234" }
+
+    @PostMapping("/write")
+    public boolean write(@RequestBody BoardDto boardDto) {   // body 데이터를 받기 위한 @RequestBody
+        System.out.println("BoardController.write");        // soutm : 메소드명 출력함수 자동완성
+        System.out.println("boardDto = " + boardDto);       // soutp : 메소드 매개변수 출력함수 자동완성
+        boolean result = BoardDao.getInstance().write(boardDto);
+        return result;
+    }
+
+    // 2. 게시물 전체 조회     [CRUD 중에 R, HTTP METHOD 중에 GET]
+    // [2] 함수위에 @XXXMapping("/http 주소 만들기")
+    // [Test] Talend Api Tester : [GET] https://localhost:8080/findall
+    @GetMapping("/findall")
+    public ArrayList<BoardDto> findAll() {
+        ArrayList<BoardDto> result = BoardDao.getInstance().findAll();
+        return result;
+    }
+
+    // 3. 게시물 개별 조회 [CRUD 중에 R, HTTP METHOD 중에 GET], 누구를 조회할지 bno가 매개변수로 필요로 한다.
+    @GetMapping("/findid")
+    // [Test] Talend Api Tester : [GET] https://localhost:8080/findid?bno=조회할 번호
+    public BoardDto findId(@RequestParam int bno) {
+        System.out.println("BoardController.findId");
+        System.out.println("bno = " + bno);
+        BoardDto result = BoardDao.getInstance().findId(bno);
+        return result;
+    }
+
+
+    // 4. 게시물 수정 [CRUD 중에 U, HTTP METHOD 중에 PUT]
+    // [TEST] Talend Api Tester : [PUT] http://localhost:8080/update
+    //                            [Headers] Content-Type : application/json
+    //                            [body] {"bno" : 3, btitle" : "수정제목" , "bcontent" : "수정내용"}
+    @PutMapping("/update")
+    public boolean update(@RequestBody BoardDto boardDto) {
+        System.out.println("BoardController.update");
+        System.out.println("boardDto = " + boardDto);
+        boolean result = BoardDao.getInstance().update(boardDto);
+        return result;
+    }
+
+    // 5. 게시물 삭제 [ CRUD 중에 D , HTTP METHOD 중에 DELETE , 쿼리스트링 ]
+    @DeleteMapping("/delete")
+    // [TEST] Talend Api Tester : [Delete] http://localhost:8080/delete?bno=1
+    public boolean delete(@RequestParam int bno) {
+        System.out.println("BoardController.delete");
+        System.out.println("bno = " + bno);
+        boolean result = BoardDao.getInstance().delete(bno);
+        return result;
+    }
+
+}
---
+++ src/main/java/day54/boardservice13/model/BoardDao.java
@@ -0,0 +1,165 @@
+package day54.boardservice13.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BoardDao {
+
+    private Connection conn;    // 연동 인터페이스
+
+    // 싱글톤
+    private static BoardDao instance = new BoardDao();
+    private BoardDao(){
+        // ========== DB 연동 ========== //
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mydb0102",
+                    "root", "1234");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static BoardDao getInstance() {
+        return instance;
+    }
+
+    // 1. 게시물 쓰기
+    public boolean write(BoardDto boardDto) {
+
+        try{
+            // 1. SQL 작성한다.
+            String sql = "insert into board(btitle, bcontent, bwriter, bpwd) values(?, ?, ?, ?)";
+            // 2. 작성한 SQL를 기재한다.
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 3. 기재된 SQL의 첫번째/두번째/세번째/네번째 ? 매개변수 값 대입
+            ps.setString(1, boardDto.getBtitle());
+            ps.setString(2, boardDto.getBcontent());
+            ps.setString(3, boardDto.getBwriter());
+            ps.setString(4, boardDto.getBpwd());
+
+            // 4. SQL 실행 => 실행된 결과 레코드 수 변환
+            int count = ps.executeUpdate();
+            // 5. 삽입된 결과 레코드가 1개 이면 등록 성공 true
+            if(count == 1) {return true;}
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // 5. 등록 실패 false
+        return false;
+    }
+
+    // 2. 게시물 전체 조회
+    public ArrayList<BoardDto> findAll() {
+
+        ArrayList<BoardDto> list = new ArrayList<>();
+
+        try{
+            // 1. SQL를 작성한다.
+            String sql = "select * from board";
+            // 2. SQL를 기재한다.
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 3. SQL 실행하고 결과를 조작하는 ResultSet 인터페이스로 받는다.
+            ResultSet rs = ps.executeQuery();
+            // 4. 조회 결과 레코드 하나씩 조회하기 / .next() : 다음 레코드 이동
+            while (rs.next()) {
+                // 5. 조회 레코드를 DTO로 만들기
+                BoardDto boardDto = new BoardDto(
+                        // 현재 조회중인 레코드의 특정 속성의 값 반환, rs.get타입("필드명")
+                        rs.getInt("bno"),
+                        rs.getString("btitle"),
+                        rs.getString("bcontent"),
+                        rs.getString("bdate"),
+                        rs.getString("bwriter"),
+                        null    // 패스워드는 보안상 조회시 제외
+                );
+                //  6. 만든 DTO에 ArrayList 에 대입
+                list.add(boardDto);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // 7. 리스트 반환
+        return list;
+    }
+
+    // 3. 게시물 개별 조회
+    public BoardDto findId(int bno) {
+
+        try{
+
+            // (1) SQL 작성한다.
+            String sql = "select * from board where bno = ?";
+            // (2) SQL 기재한다.
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // (3) 기재된 SQL을 매개변수로 대입한다.
+            ps.setInt(1, bno);
+            // (4) 기재된 SQL 실행하고 결과를 반환받는다.
+            ResultSet rs = ps.executeQuery();
+            // (5) 실행결과에 따른 제어를 한다.
+            if (rs.next()) {
+                BoardDto boardDto = new BoardDto(
+                        // 현재 조회중인 레코드의 특정 속성의 값 반환, rs.get타입("필드명")
+                        rs.getInt("bno"),
+                        rs.getString("btitle"),
+                        rs.getString("bcontent"),
+                        rs.getString("bdate"),
+                        rs.getString("bwriter"),
+                        null    // 패스워드는 보안상 조회시 제외
+                );
+                return boardDto;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    // 4. 게시물 수정
+    public boolean update(BoardDto boardDto) {
+
+        try{
+            String sql = "update board set btitle = ?, bcontent = ? where bno = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, boardDto.getBtitle());
+            ps.setString(2, boardDto.getBcontent());
+            ps.setInt(3, boardDto.getBno());
+            int count = ps.executeUpdate();
+            if (count == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return false;
+    }
+
+
+    // 5. 게시물 삭제
+    public boolean delete(int bno) {
+
+        try {
+
+            // (1) SQL 작성한다.
+            String sql = "select * from board where bno = ?";
+            // (2) SQL 기재한다.
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // (3) 기재된 SQL 의 매개변수를 대입한다.
+            ps.setInt(1, bno);
+            // (4) 기재된 SQL 실행하고 결과를 반환받는다.
+            int count = ps.executeUpdate();
+            // (5) 실행결과에 따른 제어한다.
+            if (count == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return false;
+    }
+}
